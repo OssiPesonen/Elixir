1dafcfcad5282e5b3ff781bf3089d4dba0b41055
Ossi Pesonen <ossi.pesonen@ropocapital.fi>
2020-03-06 21:07:21 +0200

    Added tests for CompositeExpression class and ExpressionBuilder class

---
+++ tests/CompositeExpressionTest.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Elixir;
+
+use Elixir\CompositeExpression;
+use PHPUnit\Framework\TestCase;
+
+class CompositeExpressionTest extends TestCase
+{
+    public function testCount()
+    {
+        $expr = new CompositeExpression(CompositeExpression::TYPE_OR, array('u.group_id = 1'));
+
+        self::assertCount(1, $expr);
+
+        $expr->add('u.group_id = 2');
+
+        self::assertCount(2, $expr);
+    }
+
+    public function testAdd()
+    {
+        $expr = new CompositeExpression(CompositeExpression::TYPE_OR, array('u.group_id = 1'));
+
+        self::assertCount(1, $expr);
+
+        $expr->add(new CompositeExpression(CompositeExpression::TYPE_AND, array()));
+
+        self::assertCount(1, $expr);
+
+        $expr->add(new CompositeExpression(CompositeExpression::TYPE_OR, array('u.user_id = 1')));
+
+        self::assertCount(2, $expr);
+
+        $expr->add(null);
+
+        self::assertCount(2, $expr);
+
+        $expr->add('u.user_id = 1');
+
+        self::assertCount(3, $expr);
+    }
+
+    /**
+     * @dataProvider provideDataForConvertToString
+     */
+    public function testCompositeUsageAndGeneration($type, $parts, $expects)
+    {
+        $expr = new CompositeExpression($type, $parts);
+
+        self::assertEquals($expects, (string) $expr);
+    }
+
+    public function provideDataForConvertToString()
+    {
+        return array(
+            array(
+                CompositeExpression::TYPE_AND,
+                array('u.user = 1'),
+                'u.user = 1'
+            ),
+            array(
+                CompositeExpression::TYPE_AND,
+                array('u.user = 1', 'u.group_id = 1'),
+                '(u.user = 1) AND (u.group_id = 1)'
+            ),
+            array(
+                CompositeExpression::TYPE_OR,
+                array('u.user = 1'),
+                'u.user = 1'
+            ),
+            array(
+                CompositeExpression::TYPE_OR,
+                array('u.group_id = 1', 'u.group_id = 2'),
+                '(u.group_id = 1) OR (u.group_id = 2)'
+            ),
+            array(
+                CompositeExpression::TYPE_AND,
+                array(
+                    'u.user = 1',
+                    new CompositeExpression(
+                        CompositeExpression::TYPE_OR,
+                        array('u.group_id = 1', 'u.group_id = 2')
+                    )
+                ),
+                '(u.user = 1) AND ((u.group_id = 1) OR (u.group_id = 2))'
+            ),
+            array(
+                CompositeExpression::TYPE_OR,
+                array(
+                    'u.group_id = 1',
+                    new CompositeExpression(
+                        CompositeExpression::TYPE_AND,
+                        array('u.user = 1', 'u.group_id = 2')
+                    )
+                ),
+                '(u.group_id = 1) OR ((u.user = 1) AND (u.group_id = 2))'
+            ),
+        );
+    }
+}
